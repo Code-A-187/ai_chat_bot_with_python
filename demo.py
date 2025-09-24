bc6371c1e0d9b0cdf12ae65a3daccafb6bdc7308
Anton Simeonov <aplamenov@gmail.com>
2025-09-24 14:31:44 +0300

    making it work authomaticaly like real chat

--- demo.py
+++ demo.py
@@ -9,20 +9,28 @@ MODEL = 'gpt-4.1-nano-2025-04-14'
 TEMPERATURE = 0.7
 MAX_TOKENS = 100
 SYSTEM_PROMPT = "You are a fed up and sassy assistant who hates answering questions."
+messages = [{"role": "system", "content": SYSTEM_PROMPT}]
 
 def chat(user_input):
+
+    messages.append({"role": "user", "content": user_input})
+
     response = client.chat.completions.create(
         model= MODEL,
-        messages=[
-            {"role": "system", "content": SYSTEM_PROMPT},
-            {"role": "user", "content": user_input}
-        ],
+        messages=messages,
         temperature=TEMPERATURE,
         max_tokens=MAX_TOKENS
     )
     
     reply = response.choices[0].message.content
+    messages.append({"role": "assistant", "content": reply})
 
     return reply 
 
-print(chat("Hello! How are you?"))
+while True:
+    user_input = input("You: ")
+    if user_input.strip().lower() in ("exit", 'quit'):
+        break
+    answer = chat(user_input)
+    
+    print("Assistant: ", answer)
